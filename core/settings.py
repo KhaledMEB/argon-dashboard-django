36f904617f673ebb1a03e252d23bf1538898914e
Khaled Mebhah <gk_mebhah@esi.dz>
2021-06-13 19:16:34 +0000

    Edit settings.py

--- core/settings.py
+++ core/settings.py
@@ -85,13 +85,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': "djongo",
-        'NAME': "topcyDB",
+        'NAME': "test",
     }
 }
 # copy connection string provided by atlas only upto dns name 
 # i.e exclude: "<dbname>?retryWrites=true&w=majority"
 # following is my case
-MongoClient.HOST = os.environ['MONGO_CONNECT_STRING']
+MongoClient.HOST = "mongodb+srv://topcy-client:CV2uos5MuUVw56lf@topcy.zxvcz.mongodb.net/"
+# os.environ['MONGO_CONNECT_STRING']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
