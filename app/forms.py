fc298d9d6fce053cea60894ff63d8afd198643b5
Khaled Mebhah <gk_mebhah@esi.dz>
2021-06-24 18:43:03 +0000

    Style the form and make it more likeable

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,32 @@ from django import forms
 
 # Create the form class.
 class PostForm(ModelForm):
+
+    produit = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder" : "e.g : Redmi Note 8",                
+                "class": "form-control"
+            }
+        ))
+    start_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'class': 'form-control'}))
+    end_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'class': 'form-control'}))
+    termes = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'placeholder' : "Les aspects qui vous intéressent le plus à propos du produis",
+                "class": "form-control",
+                'rows':4}),
+        initial = "batterie, prix, photos, qualité, support, chargeur, mémoire")
+
     class Meta:
         model = Post
         fields = ['produit', 'start_date', 'end_date', 'termes']
